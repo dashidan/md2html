84004d6083f9f6d975601899786d76a1f4a82088
SinbadFreedom <151229152@qq.com>
2018-03-23 16:29:17 +0800

    1 加入pc_pyton 2 修改template_article

--- pc_all.bat
+++ pc_all.bat
@@ -8,25 +8,3 @@ start pc_php
 start pc_redis
 start pc_webserver
 start pc_windows
-
-start amp_git
-start amp_html
-start amp_java
-start amp_linux
-start amp_mongodb
-start amp_nodejs
-start amp_php
-start amp_redis
-start amp_webserver
-start amp_windows
-
-start mip_git
-start mip_html
-start mip_java
-start mip_linux
-start mip_mongodb
-start mip_nodejs
-start mip_php
-start mip_redis
-start mip_webserver
-start mip_windows
